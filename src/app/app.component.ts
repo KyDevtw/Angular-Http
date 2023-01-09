3f53f4ac28a7468e29c44d7d349f19aa67f09c2f
Kurt Yang <kurt_yang@kunyoutech.com.tw>
2023-01-09 09:06:35 +0800

    restful api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { map } from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,18 +11,51 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPost()
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
-    console.log(postData);
+    // params API URL, body data
+    this.http.post(
+      'https://angular-backend-practice-37d63-default-rtdb.firebaseio.com/posts.json',
+      postData
+    ).subscribe(
+      res => {
+        console.log(res)
+      }
+    );
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPost()
   }
 
   onClearPosts() {
     // Send Http request
   }
+
+
+private fetchPost() {
+  this.http.get('https://angular-backend-practice-37d63-default-rtdb.firebaseio.com/posts.json')
+  // observerable 運算 使用 .pipe()
+  .pipe(
+    map(resData => {
+      const postArray = [];
+      for ( const key in resData ) {
+        if ( resData.hasOwnProperty(key) ) {
+          postArray.push({ ...resData[key], id: key });
+        }
+      }
+      return postArray;
+    })
+  )
+  .subscribe(
+    post => {
+      console.log(post)
+    }
+  )
+}
 }
